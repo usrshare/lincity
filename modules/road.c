2706698fa0e921bc89e717bd16f6146204fb03cb
/usr/share <d1337r@post.ru>
2015-06-13 12:37:03 +0300

    fix for cmake empty build

--- modules/road.c
+++ modules/road.c
@@ -4,7 +4,7 @@
  * Lincity is copyright (c) I J Peters 1995-1997, (c) Greg Sharp 1997-2001.
  * (c) Corey Keasling, 2004
  * ---------------------------------------------------------------------- */
-#include <config.h>
+//#include <config.h>
 #include <lin-city.h>
 #include <lctypes.h>
 #include <engglobs.h>
