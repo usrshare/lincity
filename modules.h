6944333d64499ea356f510cf61da97b713349e56
/usr/share <d1337r@post.ru>
2015-06-13 07:58:18 +0300

    importing all

---
+++ modules.h
@@ -0,0 +1,50 @@
+/* ---------------------------------------------------------------------- *
+ * modules.h
+ * This file is part of lincity.
+ * Lincity is copyright (c) I J Peters 1995-1997, 
+ * (c) Greg Sharp 1997-2001, (c) Corey Keasling, 2004
+ * ---------------------------------------------------------------------- */
+
+/* This is a meta-header file, which includes the headers for all of the
+   modules in modules/
+*/
+
+#ifndef __modules_h__
+#define __modules_h__
+
+#include <modules/blacksmith.h>
+#include <modules/coalmine.h>
+#include <modules/coal_power.h>
+#include <modules/commune.h>
+#include <modules/cricket.h>
+#include <modules/fire.h>
+#include <modules/firestation.h>
+#include <modules/health_centre.h>
+#include <modules/heavy_industry.h>
+#include <modules/light_industry.h>
+#include <market.h>
+#include <modules/mill.h>
+#include <modules/monument.h>
+#include <modules/oremine.h>
+#include <modules/organic_farm.h>
+#include <modules/parkland.h>
+#include <modules/port.h>
+#include <modules/pottery.h>
+#include <modules/power_line.h>
+#include <modules/rail.h>
+#include <modules/recycle.h>
+#include <modules/residence.h>
+#include <modules/road.h>
+#include <modules/rocket_pad.h>
+#include <modules/school.h>
+#include <modules/shanty.h>
+#include <modules/solar_power.h>
+#include <modules/substation.h>
+#include <modules/tip.h>
+#include <modules/track.h>
+#include <modules/university.h>
+#include <modules/water.h>
+#include <modules/windmill.h>
+
+
+#endif /* __modules_h__ */
