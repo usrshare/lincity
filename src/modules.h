f2d83738a24a30ecada3814c59edd366702e2fe3
usrshare <uls6502@tuta.io>
2015-06-19 10:52:43 +0300

    moved sources to src/

---
+++ src/modules.h
@@ -0,0 +1,50 @@
+/* ---------------------------------------------------------------------- *
+ * modules.h
+ * This file is part of lincity.
+ * Lincity is copyright (c) I J Peters 1995-1997, 
+ * (c) Greg Sharp 1997-2001, (c) Corey Keasling, 2004
+ * ---------------------------------------------------------------------- */
+
+/* This is a meta-header file, which includes the headers for all of the
+   modules in modules/
+*/
+
+#ifndef __modules_h__
+#define __modules_h__
+
+#include <blacksmith.h>
+#include <coalmine.h>
+#include <coal_power.h>
+#include <commune.h>
+#include <cricket.h>
+#include <fire.h>
+#include <firestation.h>
+#include <health_centre.h>
+#include <heavy_industry.h>
+#include <light_industry.h>
+#include "market.h"
+#include <mill.h>
+#include <monument.h>
+#include <oremine.h>
+#include <organic_farm.h>
+#include <parkland.h>
+#include <port.h>
+#include <pottery.h>
+#include <power_line.h>
+#include <rail.h>
+#include <recycle.h>
+#include <residence.h>
+#include <road.h>
+#include <rocket_pad.h>
+#include <school.h>
+#include <shanty.h>
+#include <solar_power.h>
+#include <substation.h>
+#include <tip.h>
+#include <track.h>
+#include <university.h>
+#include <water.h>
+#include <windmill.h>
+
+
+#endif /* __modules_h__ */
